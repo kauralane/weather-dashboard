e01692029e34dc27745dbf24b87b5b49ff683f25
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-01-12 19:02:47 +0000

    added initial fetch call to convert city name into coordinates, then run fetch call with those coordinates.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weather-dashboard
 An application to view 5-day weather forecasts for any city across the globe. 
+
+# Credits
+CSS reset sheet: github.com/necolas/normalize.css
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 const APIkey = "a69ab5ecb3b3c6e1ed7ac0baa202eabb"
+
+// On submit, use the coordinates API key to turn the city name that the user has inputted into latitude and longitude. Then run a fetch with the base API key, using those coordinates. 
+
+$('#search-form').on('submit', function (e) {
+    e.preventDefault();
+
+    const cityInput = $('#search-input').val().trim();
+
+    const coordURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=${APIkey}`
+
+    fetch(coordURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+    let lat = console.log(data[0].lat)
+    let lon = console.log(data[0].lon)
+
+    const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&units=metric&appid=${APIkey}`
+
+        fetch(baseURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            })
+    })
+})
+
+
