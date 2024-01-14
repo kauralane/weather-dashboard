f5d26deba7b18f36b6b17d2c2b70e0f05c961d3d
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-01-14 12:51:34 +0000

    started code to save search history to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,14 @@ $(function () {
 
     $('#search-form').on('submit', function (e) {
         e.preventDefault();
+
+        // Empty out the previous search results (if any), and make the today and 5 day forecast headings visible
+        $('#today').empty()
+        $('#forecast').empty()
         $('.forecast-heading').css('visibility', 'visible');
 
-
         let cityInput = $('#search-input').val().trim();
+        saveSearch(cityInput);
 
         const coordURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=${APIkey}`
 
@@ -23,12 +27,37 @@ $(function () {
             })
     })
 
+    // run functions when clicking previous search history button - get city name from local storage
+    // $('#cityButton').on('click', function (e) {
+    //     e.preventDefault();
+    //     console.log('clicked!')
+
+    //     // Empty out the previous search results (if any), and make the today and 5 day forecast headings visible
+    //     $('#today').empty()
+    //     $('#forecast').empty()
+    //     $('.forecast-heading').css('visibility', 'visible');
+
+    //     let prevCity = localStorage.getItem('cityButton')
+
+    //     const coordURL = `http://api.openweathermap.org/geo/1.0/direct?q=${prevCity}&appid=${APIkey}`
+
+    //     fetch(coordURL)
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (data) {
+    //             getLatLon(data);
+    //         })
+    // })
+
+
     function getLatLon(data) {
         let lat = data[0].lat;
         let lon = data[0].lon;
         getWeatherToday(lat, lon);
         getWeatherForecast(lat, lon)
     }
+
 
     function getWeatherToday(lat, lon) {
         const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`
@@ -38,11 +67,6 @@ $(function () {
                 return response.json();
             })
             .then(function (data) {
-
-                // on clicking this button again though, it will need to call out everything from local storage? Will need to save search to local storage somehow. 
-                const cityBtn = $('<button>').text(cityInput);
-                $('#history').append(cityBtn);
-
                 let cityName = $('<h3>').text(`City: ${data.city.name}`);
                 let date = $('<p>').text(`Date and time: ${data.list[0].dt_txt}`);
                 let iconCode = (data.list[0].weather[0].icon);
@@ -50,7 +74,7 @@ $(function () {
                 //add icon code to the relevant URL and set it as the img source
                 let icon = $('<img>').attr('src', `https://openweathermap.org/img/wn/${iconCode}@2x.png`)
 
-                let temp = $('<p>').text(`Temperature: ${data.list[0].main.temp}`);
+                let temp = $('<p>').text(`Temperature (celcius): ${data.list[0].main.temp}`);
                 let humidity = $('<p>').text(`Humidity: ${data.list[0].main.humidity}`);
                 let wind = $('<p>').text(`Wind speed: ${data.list[0].wind.speed}`);
 
@@ -61,6 +85,7 @@ $(function () {
             })
     }
 
+    // get 5 day weather forecast
 function getWeatherForecast(lat, lon) {
 const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`
 
@@ -70,15 +95,15 @@ const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon
         })
         .then(function (data) {
 
+// for loop to find the forecast for the same time point at each day; in this case it is every 8th object in the 'list' array from the API
             for (let i = 0; i < data.list.length; i += 8) {
-                console.log(data.list[i].dt_txt)
             let date = $('<p>').text(`Date and time: ${data.list[i].dt_txt}`);
             let iconCode = (data.list[i].weather[0].icon);
 
             //add icon code to the relevant URL and set it as the img source
             let icon = $('<img>').attr('src', `https://openweathermap.org/img/wn/${iconCode}@2x.png`)
 
-            let temp = $('<p>').text(`Temperature: ${data.list[i].main.temp}`);
+            let temp = $('<p>').text(`Temperature (celcius): ${data.list[i].main.temp}`);
             let humidity = $('<p>').text(`Humidity: ${data.list[i].main.humidity}`);
             let wind = $('<p>').text(`Wind speed: ${data.list[i].wind.speed}`);
 
@@ -87,6 +112,24 @@ const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon
 })
 }
 
+function saveSearch(cityInput) {
 
+    let cityButton = $('<button>').text(cityInput).attr('id', 'cityButton');
+    $('#history').append(cityButton);
+
+let cityNames = localStorage.getItem('cityButton') || [];
+
+    localStorage.setItem('cityButton', cityInput)
+
+    $('#cityButton').on('click', function (e) {
+        console.log('clicked')
+    })
+
+}
+
+// function renderHistory() {
+//     localStorage.getItem('cityButton')
+
+// }
 
 })
